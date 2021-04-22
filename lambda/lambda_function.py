d23eec6cb7ae88a891b2bfb69d3dbbae610f2c9e
Tim Poerschke <info@coder-tim.de>
2021-04-22 12:17:19 +0200

    Test

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -76,7 +76,7 @@ class CaptureCategoryIntentHandler(AbstractRequestHandler):
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
+                .speak("JO!")
                 # .ask("add a reprompt if you want to keep the session open for the user to respond")
                 .response
         )
