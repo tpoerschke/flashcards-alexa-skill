53a4784aabfbaf81c7d4f5c03b36cce72a5a2453
Tim Poerschke <info@coder-tim.de>
2021-04-22 13:41:33 +0200

    reprompt

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -60,7 +60,7 @@ class StartTestIntentHandler(AbstractRequestHandler):
         return (
             handler_input.response_builder
                 .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .ask(speak_output)
                 .response
         )
 
