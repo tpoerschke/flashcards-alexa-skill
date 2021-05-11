a7fbf28591f3d2e625a246ec06ffa2a1a4b5954b
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-05-11 09:08:06 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -34,7 +34,7 @@ GENERIC_ERROR_MESSAGE = "Leider ist ein unerwarteter Fehler aufgetreten. Versuch
 categories = []
 flashcards = []
 current_card = 0
-test_started = false
+test_started = False
 
 class LaunchRequestHandler(AbstractRequestHandler):
     """Handler for Skill Launch."""
