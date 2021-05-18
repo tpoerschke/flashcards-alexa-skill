8ae975300a8a8cd8a65b85aa6f976333b975fc23
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-05-18 12:12:18 +0000

    updated lambda/lambda_function.py.

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -144,15 +144,8 @@ class ShowCardBackIntentHandler(AbstractRequestHandler):
         response_builder = handler_input.response_builder.speak(speak_output)
         if test_started:
             response_builder = response_builder.ask(speak_output)
-        else:
-            
         
-        return (
-            handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
-                .response
-        )
+        return response_builder.response
 
 class HelpIntentHandler(AbstractRequestHandler):
     """Handler for Help Intent."""
