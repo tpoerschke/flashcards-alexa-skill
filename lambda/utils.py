00dd14818013d7e5eab31a31e1b39ffea60b2f62
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-06-08 09:59:59 +0000

    updated lambda/utils.py.

--- lambda/utils.py
+++ lambda/utils.py
@@ -2,5 +2,5 @@
 def init_session_attributes_for_user(session, user_id):
     session.attributes["categories"] = []
     session.attributes["flashcards"] = []
-    current_card = 0
+    session.attributes["current_card"] = 0
     test_started = False
